ba37599a57a315b700257f6982867eb2b5d4adc3
saudahmadjuwale <saudjuwale26@gmail.com>
2026-01-24 18:48:01 +0000

    User Model and Table is created!

--- RBAC/settings.py
+++ RBAC/settings.py
@@ -27,10 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "account.User"
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
+    'accessControl',
+    'businessLogic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
+    role = models.ForeignKey("accessControl.Role",on_delete=models.SET_NULL,null=True,blank=True)
+    USERNAME_FIELD = "email"
+    
+    def __str_(self):
+        return self.email

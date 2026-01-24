73e3a4081dcb3e36166fbea61dcb57f2ec13b117
saudahmadjuwale <saudjuwale26@gmail.com>
2026-01-24 19:58:02 +0000

    Created models for accessControl APP

--- accessControl/models.py
+++ accessControl/models.py
@@ -4,4 +4,17 @@ class Role(models.Model):
     name = models.CharField(max_length=50,unique=True)
 
     def __str__(self):
-        return self.name
+        return self.role_name
+class Permission(models.Model):
+    jobs = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.jobs
+class RolePermission(models.Model):
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission,on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('role','permission')
+    def __str__(self):
+        return f"{self.role} -> {self.permission}"

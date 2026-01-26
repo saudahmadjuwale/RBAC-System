0e4e8b7bb6e8882137f9ccc7c43a55651b1f33fa
saudahmadjuwale <saudjuwale26@gmail.com>
2026-01-26 23:00:31 +0000

    We replace the django is_staff permission check to our own decorator named require permission which checks the users are allowed to access that particular action or not. For now i have only done the create_user decorators not even assigned the roles and perssions to it !

---
+++ accessControl/decorators.py
@@ -0,0 +1,25 @@
+from django.http HttpResponseForbidden
+from functools import wraps
+
+def require_permission(permission_name):
+    def decorators(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request,*args, **kwargs):
+            user = request.user
+
+            if not user.is_authenticated:
+                return HttpResponseForbidden("You are not auhtenticated !")
+            
+            if not user.role:
+                return HttpResponseForbidden("You don't have any role !")
+            
+            has_permission = user.role.permission.filter(name=permission_name).exist()
+
+            if not has_permission:  
+                return HttpResponseForbidden(f"You don't have permission access {permission_name} !")
+            
+            return view_func(request,*args,**kwargs)
+        return wrapped_view
+    return decorators
+
+

5f06573400a7476579919b55a9d1acc6c539946e
saudahmadjuwale <saudjuwale26@gmail.com>
2026-01-30 16:47:57 +0000

    Solved some small mistakes

--- accessControl/views.py
+++ accessControl/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from accessControl.decorators import require_permission
-from .models import Permission
+from .models import Permission, Role
+
 
 @login_required
 @user_passes_test(lambda u: u.is_staff)
 def create_permission(request):
     if request.method == "POST":
         name = request.POST.get('name')
-
         if Permission.objects.filter(jobs=name).exists():
             messages.error(request,"The Permission already exist!")
         else:
